6627af78a1e38223966d28f257932f112dfc3df2
Goncear Valera <valeragoncear@gmail.com>
2024-02-02 09:41:35 +0200

    version 1.0.2

--- js/main.js
+++ js/main.js
@@ -3,12 +3,12 @@ const menuClose = document.querySelector('.menu__close');
 const menuList = document.querySelector('.menu__list');
 const menuShadow = document.querySelector('.menu--close')
 
-menuBtn.addEventListener('click', ()=>{
+menuBtn.addEventListener('click', () => {
     menuList.classList.toggle('menu__list--open');
     menuShadow.classList.toggle('menu--open');
 });
 
-menuClose.addEventListener('click', ()=>{
+menuClose.addEventListener('click', () => {
     menuList.classList.remove('menu__list--open');
     menuShadow.classList.remove('menu--open');
 });
